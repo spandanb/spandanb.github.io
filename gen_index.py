4dfd34140b4e12c7a9deaf6f35d3bc1586376ae7
spandanb <spandan.777@gmail.com>
2017-01-07 19:36:48 -0500

    Using jinja to generate index.html

---
+++ gen_index.py
@@ -0,0 +1,22 @@
+from __future__ import print_function
+
+"""
+Generates index.html from contents and index.template.html
+"""
+
+import yaml
+from jinja2 import Template
+
+if __name__ == "__main__":
+    #Read the content 
+    with open('content.yaml', 'r') as stream:
+        content = yaml.load(stream)
+
+    #Read the template
+    with open('index.template.html') as f:
+        template = f.read()
+
+    #Render the output 
+    t = Template(template) 
+    with open('index.html', 'w') as f:
+        f.write(t.render(blogposts=content['blogposts'], projects=content['projects']))
