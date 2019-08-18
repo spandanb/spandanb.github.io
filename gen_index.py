582df28a044d5457cb7681fb59beb0ed34dbce26
Spandan Bemby <spandan.777@gmail.com>
2019-08-17 19:58:31 -0700

    Document website generation; template blurb and email; use Pipfile

--- gen_index.py
+++ gen_index.py
@@ -10,15 +10,16 @@ from jinja2 import Template
 import pdb
 
 if __name__ == "__main__":
-    #Read the content 
+    #Read the content
     with open('content.yaml', 'r') as stream:
-        content = yaml.load(stream)
+        content = yaml.load(stream, Loader=yaml.FullLoader)
 
     #Read the template
     with open('index.template.html') as f:
         template = f.read()
 
-    #Render the output 
-    t = Template(template) 
+    #Render the output
+    t = Template(template)
     with open('index.html', 'w') as f:
-        f.write(t.render(blogposts=content['blogposts'], projects=content['projects']))
+        f.write(t.render(blurb=content['blurb'], email=content['email'],
+                         blogposts=content['blogposts'], projects=content['projects']))
